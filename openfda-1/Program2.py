67b2d222b58639a196ffe70cbbce003bc2ac6cba
weamait <weamouh@gmail.com>
2018-03-26 14:57:13 +0200

    Actualizando Program2

---
+++ openfda-1/Program2.py
@@ -0,0 +1,17 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?&limit=11", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+repo = repos['results']
+for elem in repo:
+    print("El identificador es", repo[elem]['id'])
