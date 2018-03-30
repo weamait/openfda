df25798823bac0427bd91b434af3d213e9bb5c3e
weamait <weamouh@gmail.com>
2018-03-30 11:22:44 +0200

    Actualizando

--- openfda-1/Prueba2.py
+++ openfda-1/Prueba2.py
@@ -10,3 +10,21 @@ cont = json.loads(r.decode('utf-8'))
 
 for item in cont['results']:
     print("El identificador es:", item['id'])
+
+
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?&limit=11", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+for elem in repos['results']:
+    print("El identificador es:", elem['id'])
