c61079cbaedcd431887e81c0db63782cf95a8b91
weamait <weamouh@gmail.com>
2018-04-01 21:43:00 +0200

    Actualizando

--- openfda-2/practica2.py
+++ openfda-2/practica2.py
@@ -3,8 +3,8 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?&search=active_ingredient:%22acetylsalicylic+acid%22&limit=100", None, headers)
+conn = http.client.HTTPSConnection('api.fda.gov')
+conn.request('GET', '/drug/label.json?&search=active_ingredient:"acetylsalicylic+acid"&limit=100', None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
@@ -13,4 +13,7 @@ conn.close()
 repos = json.loads(repos_raw)
 
 for elem in repos['results']:
-    print("Los nombres de todos los fabricantes que producen aspirinas son:", elem['openfda']['manufacturer_name'])
+    try:
+        print("Los nombres de todos los fabricantes que producen aspirinas son:", elem['openfda']['manufacturer_name'])
+    except KeyError:
+        print("El medicamento carece de empresa")
