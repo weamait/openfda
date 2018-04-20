6aeb6c512409604ca6a403d872a5534ea8547d2b
weamait <weamouh@gmail.com>
2018-04-20 18:20:28 +0200

    Últimos retoques

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -1,1 +1,14 @@
+from flask import Flask
+from flask import request
 
+app = Flask(__name__)
+
+
+@app.route("/clase")
+def hello():
+    limite = request.args.get('limit')
+    return "Hello World!"+limite
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8000)
