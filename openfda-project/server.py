d20395d00f900a07432b144a13a0590c8bc0fb5b
weamait <weamouh@gmail.com>
2018-05-04 17:50:12 +0200

    Últimos retoques

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -4,10 +4,52 @@ from flask import request
 app = Flask(__name__)
 
 
-@app.route("/clase")
+@app.route("/searchDrug")
 def hello():
-    limite = request.args.get('limit')
-    return "Hello World!"+limite
+    ingactivo = request.args.get('active_ingredient')
+    return "Hello World!"+ingactivo
+
+@app.route("/searchCompany")
+def buscar_empresa():
+    empresa = request.args.get('company')
+    return empresa
+
+@app.route("/listDrugs")
+def lista_medicamentos():
+    medicamentos = request.args.get("generic_name")
+    return medicamentos
+
+@app.route("/listCompanies")
+def lista_empresas():
+    empresas = request.args.get("manufacturer_name")
+    return empresas
+
+@app.route("/")
+def paginaHTML(): #crear página web con formularios o alguna pregunta o lo que sea.
+    contenido = """
+          <!DOCTYPE html>
+            <html>
+            <body>
+            <body style='background-color: turquoise'>
+
+            <h2>HTML Forms</h2>
+
+            <form action="/action_page.php">
+              Search Drug:<br>
+              <input type="text" name="searchDrug">
+              <br>
+              Search company:<br>
+              <input type="text" name="searchCompany">
+              <br><br>
+              <input type="submit" value="Submit">
+            </form> 
+
+            <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+
+            </body>
+            </html>"""
+    return contenido
+
 
 
 if __name__ == "__main__":
